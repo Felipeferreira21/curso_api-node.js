80676b7e82eade9b364eb90d32a5d3b03af0d3db
felipeferreira21 <felipeferre26@hotmail.com>
2023-07-05 11:00:03 -0300

    Criando banco de dados

--- src/app.js
+++ src/app.js
@@ -1,23 +1,32 @@
 import express, { json } from "express";
+import db from "./config/dbConnect.js";
+import livros from "./models/Livro.js";
 
+
+db.on("error", console.log.bind(console, 'Erro de conexão'))
+db.once("open", () => {
+    console.log('Conexão com o banco feita com sucesso')
+})
 
 const app = express();
 
 app.use(express.json());
-
-const livros = [
-    { id: 1, "titulo": "Pai rico, Pai pobre" },
-    { id: 2, "titulo": "Manual da Eficiencia" }
-]
 
 app.get('/', (req, res) => {
     res.status(200).send("Curso de Node");
 })
 
 app.get('/livros', (req, res) => {
-    res.status(200).json(livros);
-})
-
+    livros.find()
+      .then((livros) => {
+        res.status(200).json(livros);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Erro ao buscar os livros");
+      });
+  });
+  
 app.get('/livros/:id', (req, res) => {
     let index = buscaId(req.params.id);
     res.json(livros[index]);
